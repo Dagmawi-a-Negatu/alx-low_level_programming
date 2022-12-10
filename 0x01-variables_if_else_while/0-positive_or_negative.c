02dc78a88184105e24e4f93cd6d8a7578165be3c
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-10 06:36:03 -0800

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**more headers goes there
+/*
  * printf function definition
  * For some builtin function
  * For time builtin function
  */
 
-/**betty style doc for function main goes there 
+/* 
  * main - Check if number is pos, neg or zero
  *
- * Description: This is a longer description
- * Generate random number
- * Check generated number value
- * Print if number is positive, negative or zero
  * Return: 0 for main integer return functionality
  */
 int main(void)
