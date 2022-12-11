44fc24ba7aef4fc7470e15cb0b84d4ea343048ff
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-11 00:45:08 -0800

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * printf - Print sentence with commas
+ *
+ * Return: 0 for printf integer return
+ */
+
+int printf(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
