511364ce8f73b4df4aee767b5fe625e5dd2ebb44
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-10 23:27:10 -0800

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Print all single digit numbers using puthchar
+ *
+ * Return: 0 for main functionality
+ */
+
+int main(void)
+{
+	putchar('1');
+	putchar('2');
+	putchar('3');
+	putchar('4');
+	putchar('5');
+	putchar('6');
+	putchar('7');
+	putchar('8');
+	putchar('9');
+	putchar('\n');
+	return (0);
+}
