b33fff79981ffeae04404517a7ed86d94ce74b29
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-10 23:18:49 -0800

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print all single digit numbers of base 10
+ *
+ * Return: 0 for main functionality
+ */
+
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
