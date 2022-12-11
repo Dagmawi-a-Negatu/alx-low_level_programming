40f801cc29e93947d01229b030c2f61f7b912ec5
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-10 22:28:34 -0800

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet in lowercase
+ *
+ * Return: 0 for main interger return functionality
+ */
+
+int main(void)
+{
+
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+		putchar('\n');
+	return (0);
+}
