f34577fa0156cbdd7f9bb91e5a14b58036242831
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-13 05:42:59 -0800

    test

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,1 @@
-#include <stdio.h>
 
-/**
- * print_alphabet - print all alphabets in lowercase
- *
- */
-void print_alphabet(void)
-{
-	int i;
-
-	for (i = '0'; i <= 'z'; i++)
-		putchar(i);
-	putchar('\n');
-}
