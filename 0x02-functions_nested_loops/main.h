52735d9c2eb2f564381cbcb6bd57840c50e23b34
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-13 05:49:10 -0800

    header

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,3 +11,16 @@ int main(void)
 	printf("_putchar\n");
 	return (0);
 }
+
+/**
+ * print_alphabet - print all lowercase letters
+ *
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i ++)
+		putchar(i);
+	putchar('\n');
+}
