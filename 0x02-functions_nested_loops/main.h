5acc627b32607c54ae0ba9ae0241090cbf4f2199
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-13 04:34:30 -0800

    head

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putschar followed by a new line
+ *
+ * Return: 0 for interger return functionality
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
