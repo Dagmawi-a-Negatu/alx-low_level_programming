0319215342cd09bd844f035c4b5b0c4e90eb5b9f
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-13 06:49:58 -0800

    check

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,19 @@
+#include <stdio.h>
 #include "main.h"
 /*
- *main. h file for print alphabet function
+ *main. h file for print prototype function delceration
  */
 
-print_alphabet();
-	
+/**
+ * print_alphabet - print lowecase letters followed by a new line
+ *
+ */
 
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	puthcar('\n');
+}
