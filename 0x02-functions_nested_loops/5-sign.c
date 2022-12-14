0f4d91e5ecf37bfe7fe3ef8db7a1be5f351e0302
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-14 05:55:37 -0800

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,14 +15,8 @@
 int print_sign(int n)
 {
 	if (n > 0)
-	{
-		return ('+', 1);
-	}
+		return ("+1");
 	if (n < 0)
-	{
-		printf("-");
-		return (-1);
-	}
-	printf("0");
-	return (0);
+		return ("-1");
+	return ("00");
 }
