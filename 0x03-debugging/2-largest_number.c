733f1be3e9fef6f8b7cf6f92c091f8439010631c
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-15 04:39:47 -0800

    2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,11 +13,17 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	if (a > b && a > c)
+	{
 		largest = a;
+	}
 	else if (b > a && b > c)
+	{
 		largest = b;
+	}
 	else
-		largest = 4;
+	{
+		largest = b;
+	}
 
 	return (largest);
 }
