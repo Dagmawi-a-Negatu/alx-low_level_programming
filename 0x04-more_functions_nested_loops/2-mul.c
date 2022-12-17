c5097e725f20ad8f8b765a260c065ba96ed53cec
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-17 02:37:59 -0800

    task 0

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * mul- checks if character is uppercase
+ * @a: First operand
+ * @b: second operand
+ * Return: Return a multiplied by b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
