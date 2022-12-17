be2174e0e7bcd8a8bbb7d90b5a0d65d1a2bce1f0
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-17 07:08:33 -0800

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,23 +3,14 @@
 #include <ctype.h>
 
 /**
- * print_diagonal- print diagonal shape character
+ * print_line- prints numbers from 0 to 14 10 iterations
  * @n: number of times to print character
  */
-void print_diagonal(int n)
+void print_line(int n)
 {
 	int i;
-	int x;
 
-	/* x = 0; */
 	for (i = 0; i < n; i++)
-	{
-		_putchar('/');
-		_putchar('\n');
-		/* x++; */
-		/* for (int j = 0; j < x; j++) */
-			/* _putchar(' '); */
-	}
-
+		_putchar('_');
 	_putchar('\n');
 }
