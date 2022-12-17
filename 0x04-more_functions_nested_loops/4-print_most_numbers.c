7d8982b3741b995011c72240a5cab0ba60a6302b
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-17 02:57:11 -0800

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_most_numbers- prints numbers from 0 to 9 excluding 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+			putchar(i);
+	}
+	putchar('\n');
+}
