12af06abed4f98083463c539f6c67cfb95e89547
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-17 07:17:35 -0800

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,18 +8,5 @@
  */
 void print_diagonal(int n)
 {
-	int i;
-	int x;
-
-	/* x = 0; */
-	for (i = 0; i < n; i++)
-	{
-		_putchar('/');
-		_putchar('\n');
-		/* x++; */
-		/* for (int j = 0; j < x; j++) */
-			/* _putchar(' '); */
-	}
-
 	_putchar('\n');
 }
