05ca2f910c86ba74f3cd2b3f9dbf6e4711b7cb90
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-17 04:59:27 -0800

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_line- prints numbers from 0 to 14 10 iterations
+ * @n: number of times to print character
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (j = 0; j < n; j++)
+		_putchar('_');
+	_putchar('\n');}
+}
