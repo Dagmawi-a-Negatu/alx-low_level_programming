08ea3d48a8406c19e4ed2ea534251b00ee9c2da3
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-18 05:44:54 -0800

    test

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,26 +4,30 @@
 
 /**
  * print_triangle- prints numbers from 0 to 14 10 iterations
- * @size: number of times to print character in triangle shape
+ * @size: number of times to print character in square shape
  */
 void print_triangle(int size)
 {
+	int i;
+	int j;
 	int k;
 	int space;
+	int astericnum;
 
 	space = size;
-	k = 1;
+	astericnum = 1;
+	for (i = 0; i < size; i++)
+	{
+		for (k = 1; k < space; k++)
+			_putchar(' ');
+		for (j = 0; j < astericnum; j++)
+			_putchar('#');
+		space--;
+		astericnum++
+		_putchar('\n');
+	}
+	
 	if (size <= 0)
 		_putchar('\n');
-	for (int i = 0; i < size; i++)
-	{
-		for (int x = 0; x < space - 1; x++)
-			_putchar(' ');
-		for (int j = 0; j < k; j++)
-			_putchar('#');
-		_putchar('\n');
-		k++;
-		space--;
-	}
 
 }
