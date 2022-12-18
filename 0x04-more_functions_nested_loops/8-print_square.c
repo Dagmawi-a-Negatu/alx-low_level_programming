fc034795bd5fe8f77b6fee48ffbf5d875e8f65f5
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-18 00:37:11 -0800

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,7 +3,7 @@
 #include <ctype.h>
 
 /**
- * print_line- prints numbers from 0 to 14 10 iterations
+ * print_square- prints numbers from 0 to 14 10 iterations
  * @size: number of times to print character in square shape
  */
 void print_square(int size)
@@ -11,10 +11,13 @@ void print_square(int size)
 	int i;
 	int j;
 
+	if (size <= 0)
+		_putchar('\n');
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 			_putchar('#');
 		_putchar('\n');
 	}
+
 }
