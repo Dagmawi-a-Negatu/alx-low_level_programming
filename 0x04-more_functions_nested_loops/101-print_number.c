d570e3e6a1f326548babacd76215d6069bb54070
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-18 06:50:38 -0800

    101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_numbers- checks if character is uppercase
+ * @n: value to be printed
+ */
+void print_number(int n)
+{
+	_putchar(n + '0');
+}
