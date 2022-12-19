795b09c136fb0ebce164b482421b57eb64bc44c2
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-19 07:57:04 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen- write a function that returs length of string
+ * @s: value to be checked
+ *
+ * Return: return length number of pointer variable
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	count = strlen(s);
+
+	return (count);
+}
