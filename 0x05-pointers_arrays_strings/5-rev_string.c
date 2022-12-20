9b05822a6116da0c97157d1f61ee7f0abb8dd105
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-20 00:43:53 -0800

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - write a function that prints string in reverse
+ * @s: string to be printed
+ */
+
+void rev_string(char *s)
+{
+	int i;
+
+	for (i = strlen(s) - 1; i >= 0; i--)
+		putchar(s[i]);
+	putchar('\n');
+}
