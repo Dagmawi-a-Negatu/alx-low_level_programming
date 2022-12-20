2c42dba551f6d4de87248c6d10a8e167cb2813a3
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-20 07:30:52 -0800

    task 7

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_array - write a function that prints string in reverse
+ * @a: array to be printed
+ * @n: number of array elements to be printed
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n ; i--)
+		printf("%d, ", a[i]);
+	putchar('\n');
+}
