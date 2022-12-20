319fb7a254ec2239e9c0c01f8f61d9929d5b0341
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-20 06:06:50 -0800

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - write a function that prints characters
+ * @s: string to be printed
+ */
+
+void puts2(char *str);
+{
+	int i;
+
+	for (i = 0; i < strlen(str); i+2)
+		putchar(str[i]);
+	putchar('\n');
+}
