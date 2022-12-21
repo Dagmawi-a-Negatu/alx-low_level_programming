010b15d39c2588dc23b846844092b375314b250b
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-21 01:17:41 -0800

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,9 +14,10 @@ void rev_string(char *s)
 	
 	len = (int) strlen(s);
 	rev = s;
+	j = 0;
 	for (i = len - 1; i >= 0; i--)
 	{
-		for (j = 0; j < len ; j++)
-			s[j] = rev[i];
+		s[j] = rev[i];
+		j++;
 	}	
 }
