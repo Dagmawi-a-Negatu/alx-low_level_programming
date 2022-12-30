beded982cae0fa7f4b63b6a9564a9634aac99758
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-30 11:04:33 -0800

    task 1

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,9 @@
-#include <stdio.h>                                                                                                           
-void _puts(char *str);                                                          
-char *_strcat(char *dest, char *src);                                           
+#ifndef _main_H_
+#define _main_H_
+
+#include <stdio.h>
+void _puts(char *str);
+char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+
+#endif /* _main_H_ */
