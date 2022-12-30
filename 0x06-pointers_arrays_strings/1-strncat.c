e22f68a1aedc0ed9c4d2b5ab709d1cfa42767c18
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2022-12-30 10:32:51 -0800

    task 0

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strncat - concatnates two strings
+ * @dest: string to be added on
+ * @src: strubg to be appended
+ * @n: 
+ * Return: concatnated string
+ */
+char *_strncat(char *dest, char *src, int n);
+{
+	strncat(dest, src, n);
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,3 +6,4 @@
 
 void _puts(char *str);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
