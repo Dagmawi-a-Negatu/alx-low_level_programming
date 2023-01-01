5e1abd65872cd2ed7db3ee4d061a2f8ea236935b
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2023-01-01 11:24:01 -0800

    task 5

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - concatnates two strings
+ * @s1: First integer
+ * @s2: second integer
+ * Return: Integer always
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * *string_toupper - return all uppercase letters
+ * @*: string to be returned
+ * Return: string
+ */
+char *string_toupper(char *)
+{
+	return (toupper(*));
+}
