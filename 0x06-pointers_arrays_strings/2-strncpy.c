9d14abb2f21a6961b0d3ca97ff9637f99f69a705
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2023-01-01 03:13:33 -0800

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strncpy - concatnates two strings
+ * @dest: string to be added on
+ * @src: strubg to be appended
+ * @n:number of allowed bytes
+ * Return: concatnated string
+ */
+char *_strncpy(char *dest, char *src, int n);
+{
+	strncat(dest, src, n);
+	return (dest);
+}
