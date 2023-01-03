1382ddae8aa5fce7079d39b21e5e41665c81ed6f
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2023-01-03 10:23:04 -0800

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strchr - Returns a pointer first occurrence of the character c
+ * @s: string to be searched
+ * @c: character to search for in string 
+ *
+ * Return: pointer of first occurrence of character c
+ */
+
+char *_strchr(char *s, char c)
+{
+	strchr( s, c);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,4 +4,5 @@
 void _puts(char *str);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 #endif /* _main_H_ */
