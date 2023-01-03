8bad9f96a2498b1523c6334c9fbb16684ef62730
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2023-01-03 09:58:15 -0800

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_memcpy - copies memory area of src to dest
+ * @dest: pointer to be retured
+ * @src: pointer to copy memory area from
+ * @n: number of allowed bytes to copy from src
+ *
+ * Return: returns a pointer to the memory area of dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	memccpy(dest, src, n);
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,5 +3,5 @@
 #include <stdio.h>
 void _puts(char *str);
 char *_memset(char *s, char b, unsigned int n);
-
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif /* _main_H_ */
