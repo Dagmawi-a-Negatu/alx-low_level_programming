fec313da7aa505d9fe71b3e24e49871ed1ede784
Dagmawi-a-Negatu <dagmawi.negatu@gmail.com>
2023-01-03 10:43:28 -0800

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strchr - Returns initial segment of s consisting only of bytes accept
+ * @s: string parameter as initial segment
+ * @accept: strings used for number of bytes managment
+ *
+ * Return: returns the number of bytes
+ */
+unsigned int _strspn(char *s, char *accept);
+{
+	char *ret;
+
+
+	ret = strchr(s, accept);
+	return (ret);
+}
